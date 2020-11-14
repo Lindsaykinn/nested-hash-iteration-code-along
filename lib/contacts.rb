c2ba8504d687a2f82ac8d4d08169eaa70e703274
Lindsay Kinn <lindsay.kinn@gmail.com>
2020-11-14 17:38:43 -0500

    Done.

--- lib/contacts.rb
+++ lib/contacts.rb
@@ -7,6 +7,7 @@ require 'pry'
   #     email: "jon_snow@thewall.we", 
   #     favorite_ice_cream_flavors: ["chocolate", "vanilla"]
   #   },
+    
   #   "Freddy Mercury" => {
   #     name: "Freddy",
   #     email: "freddy@mercury.com",
@@ -15,5 +16,13 @@ require 'pry'
   # }
   
 def remove_strawberry(contacts)
-
+  contacts.each do |person, contact_details_hash|
+   if person == "Freddy Mercury"
+    contact_details_hash.each do |attribute, data|
+      if attribute == :favorite_ice_cream_flavors
+        data.delete_if {|ice_cream| ice_cream == "strawberry"}
+        end     
+      end
+    end
+  end
 end
